153f32d3e435c4255c4d8a33af84b51867b78dd2
Aleksandr N Potashev <sms@arh.net.ru>
2023-02-12 12:51:07 +0300

    add forgotten changes

--- bh/src/test/kotlin/ru/net/arh/vocabulary/bh/VocabularyApplicationTests.kt
+++ bh/src/test/kotlin/ru/net/arh/vocabulary/bh/VocabularyApplicationTests.kt
@@ -2,12 +2,15 @@ package ru.net.arh.vocabulary.bh
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VocabularyApplicationTests {
 
     @Test
     fun contextLoads() {
+        println()
     }
 
 }
