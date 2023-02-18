9b207c7135a22841a43b0ce37c4ef9d9d46d2eb5
Поташев Александр Николаевич <ANiPotashev@sberbank.ru>
2023-02-18 16:06:26 +0300

    feature 6: repare tests

---
+++ Run test README.md
@@ -0,0 +1,5 @@
+Tu run IT need:
+- have docker installed
+- pass environment variables BOT_NAME and BOT_TOKEN
+
+mvn -DBOT_NAME=<BOT_NAME> -DBOT_TOKEN=<BOT_TOKEN> test
--- bh/src/test/kotlin/ru/net/arh/vocabulary/bh/VocabularyApplicationTests.kt
+++ bh/src/test/kotlin/ru/net/arh/vocabulary/bh/VocabularyApplicationTests.kt
@@ -2,15 +2,49 @@ package ru.net.arh.vocabulary.bh
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.util.TestPropertyValues
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
 
 @SpringBootTest
-@ActiveProfiles("test")
+@Testcontainers
+@ContextConfiguration(initializers = [VocabularyApplicationTests.Initializer::class])
 class VocabularyApplicationTests {
+
+    companion object {
+
+        const val DB_NAME = "testdb"
+        const val DB_USER = "testdb-user"
+        const val DB_PASS = "testdb-password"
+
+        @Container
+        val pgContainer = PostgreSQLContainer<Nothing>(
+                DockerImageName.parse("postgres:14")
+        )
+                .apply {
+                    withDatabaseName(DB_NAME)
+                    withUsername(DB_USER)
+                    withPassword(DB_PASS)
+                }
+    }
+
+    internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+        override fun initialize(configurableApplicationContext: ConfigurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=jdbc:tc:postgresql:14:///vocabulary",
+                    "spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
+            ).applyTo(configurableApplicationContext.environment)
+        }
+    }
 
     @Test
     fun contextLoads() {
-        println()
     }
 
 }
